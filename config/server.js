65ac7c309158a2354c9e067d095caaa64699228c
elprofa <wedoprofan14@gmail.com>
2021-09-03 02:46:19 +0100

    first commit 2

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '13ab4780ff40e6fd3dc7ea577773c536'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'befd048d-b2ab-4061-8517-6b6f97accaa8'
+};
